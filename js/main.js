e2ab412a5d0842f20d7238dbd9589cb60dea2c76
Kattmynta <soleil_opaque@mail.ru>
2016-11-25 19:17:26 +0300

    Скрипт открытия/закрытия модального окна

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+var link = document.querySelector(".map .btn");
+var popup = document.querySelector(".modal-window");
+var close = document.querySelector(".modal-window-close");
+var form = popup.querySelector("form");
+
+// TODO: а почему тут не срабатывает form.querySelector?
+var user_name = popup.querySelector(".username input");
+var user_email = popup.querySelector(".email input");
+var letter_text = popup.querySelector("textarea");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-window-show");
+  user_name.focus();
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-window-show");
+});
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(form);
+  console.log(user_name);
+  console.log(user_email);
+  console.log(letter_text);
+
+  if (!user_email.value || !letter_text.value) {
+    event.preventDefault();
+    console.log("Oops!"); // TODO:
+  }
+});
+
 function initialize() {
   var mapOptions = {
     zoom: 16,
