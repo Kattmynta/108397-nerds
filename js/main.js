0f887dc8adda097f8b29e95dbf73b2522a3c82c0
Kattmynta <soleil_opaque@mail.ru>
2016-11-25 23:26:49 +0300

    Минификация

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-﻿var link = document.querySelector(".map .btn");
+﻿
+var link = document.querySelector(".map .btn");
 var popup = document.querySelector(".modal-window");
 var close = document.querySelector(".modal-window-close");
 var form = popup.querySelector("form");
@@ -6,8 +7,6 @@ var form = popup.querySelector("form");
 var user_name = popup.querySelector(".username input");
 var user_email = popup.querySelector(".email input");
 var letter_text = popup.querySelector("textarea");
-
-var empty_field=0;
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
@@ -22,24 +21,35 @@ close.addEventListener("click", function(event) {
 });
 
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  if (!user_name.value) empty_field = user_name;
-  else if (!user_email.value) empty_field = user_email;
-  else if (!letter_text.value) empty_field = letter_text;
-  else
-  {
+  if (!user_name.value || !user_email.value || !letter_text.value) {
+    event.preventDefault();
+    if (!user_name.value) {
+      event.preventDefault();
+      user_name.classList.add("modal-input-error");
+      if (user_name.addEventListener("focus", function(event) {
+          event.preventDefault();
+          user_name.classList.remove("modal-input-error");
+        })) {}
+    }
+    if (!user_email.value) {
+      event.preventDefault();
+      user_email.classList.add("modal-input-error");
+      if (user_email.addEventListener("focus", function(event) {
+          event.preventDefault();
+          user_email.classList.remove("modal-input-error");
+        })) {}
+    }
+    if (!letter_text.value) {
+      event.preventDefault();
+      letter_text.classList.add("modal-input-error");
+      if (letter_text.addEventListener("focus", function(event) {
+          event.preventDefault();
+          letter_text.classList.remove("modal-input-error");
+        })) {}
+    }
+  } else
     form.submit();
-    return;
-  }
-
-  empty_field.classList.add("modal-input-error");
-  empty_field.addEventListener("input", function(event) {
-    empty_field.classList.remove("modal-input-error");
-  });
-
 });
-
 
 function initialize() {
   var mapOptions = {
@@ -49,6 +59,10 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map'), mapOptions);
   var image = "img/map-marker.png";
   var myLatLng = new google.maps.LatLng(59.9388571, 30.322028);
-  var beachMarker = new google.maps.Marker({position: myLatLng, map: map, icon: image});
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
 }
 google.maps.event.addDomListener(window, "load", initialize);
